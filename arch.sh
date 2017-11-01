4273fb8b9caa8f0014c69c5272a6e77458efd75f
Ante <vuletic23@gmail.com>
2017-11-01 13:52:06 +0100

    Code refactoring

--- arch.sh
+++ arch.sh
@@ -8,11 +8,9 @@ partition()
   read -r pname
 
   if [ -b /dev/"$pname" ]; then
-    parted /dev/"$pname" mklabel GPT  
-    parted /dev/"$pname" mkpart P1 1MiB 512MiB
-    parted /dev/"$pname" mkpart P2 512MiB 100%  
-    yes | mkfs.fat -F32 /dev/"$pname"1
-    yes | mkfs.ext4 /dev/"$pname"2
+    parted /dev/"$pname" mklabel GPT
+    parted /dev/"$pname" mkpart primary fat32 1MiB 512MiB
+    parted /dev/"$pname" mkpart primary ext4 512MiB 100%      
     parted /dev/"$pname" set 1 boot on
   else
     echo -e "\nSelected device does not exist, installation interrupted.\n"
